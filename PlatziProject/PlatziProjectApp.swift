3e8a9fca2f102b9e3b432aa85717f91429cabc40
Miguel Jasso <dackerjasso@gmail.com>
2024-07-10 10:37:21 -0600

    Initial Commit

---
+++ PlatziProject/PlatziProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziProjectApp.swift
+//  PlatziProject
+//
+//  Created by Mike Jasso on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PlatziProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
